5f5e5c0f4f0cdcef4808cba8fbe3151a6a6f1ea9
Dmytro <dmytro.bor@code-care.pro>
2025-01-07 01:01:35 +0200

    implement splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,20 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.testb1g2.app',
-  appName: 'testb1g1',
-  webDir: 'out'
+  appId: "com.testb1g2.app",
+  appName: "testb1g1",
+  webDir: "out",
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1000,
+      launchAutoHide: true,
+      backgroundColor: "#FFC632",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
