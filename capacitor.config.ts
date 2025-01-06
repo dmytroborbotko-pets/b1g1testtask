d1dc81112300c1595274623eee2aa18274f54842
Dmytro <dmytro.bor@code-care.pro>
2025-01-06 16:44:59 +0200

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.testb1g2.app',
+  appName: 'testb1g1',
+  webDir: 'out'
+};
+
+export default config;
